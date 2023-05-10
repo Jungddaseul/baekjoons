45bb317a61354bb0db3d693984cdbb605342b2b7
Jung Daseul <114555218+Jungddaseul@users.noreply.github.com>
2023-05-10 11:27:37 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 131.06 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in range(0, len(arr)):
+        if i == 0:
+            answer.append(arr[i])
+        elif arr[i] != arr[i-1]:
+            answer.append(arr[i])
+
+    return answer
