8216316f8dabed5f32cc6a7b0faeb1b165aed508
Jung Daseul <114555218+Jungddaseul@users.noreply.github.com>
2023-05-12 16:02:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,12 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(n):
+        a = bin(arr1[i] | arr2[i]) #  결과 '0b11111'
+        a = a[2:].zfill(n)
+        a = a.replace('1', '#').replace('0', ' ')
+        answer.append(a)
+        
+    return answer
+    
+        
+
