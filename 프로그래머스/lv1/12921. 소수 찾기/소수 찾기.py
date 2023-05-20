ec68d49ca8efff65516cbe6e58214b5c28fe0800
Jung Daseul <114555218+Jungddaseul@users.noreply.github.com>
2023-05-20 22:20:20 +0900

    [level 1] Title: 소수 찾기, Time: 114.41 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,18 +1,16 @@
 def solution(n):
-    # 0과 1은 소수가 아니므로 초기값을 2로 설정
-    answer = 0
-    
-    # 주어진 범위 내의 숫자에 대해 소수 여부를 판별
-    for num in range(2, n+1):
-        is_prime = True
-        
-        # 2부터 현재 숫자의 제곱근까지 나누어 소수 여부 확인
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                is_prime = False
-                break
-                
-        # 소수인 경우 answer 증가        
-        if is_prime:
-            answer += 1
-    return answer
+    if n < 2:
+        return 0
+
+    # 주어진 범위 내의 숫자에 대해 소수 여부를 판별하는 리스트
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    # 2부터 숫자의 제곱근까지 소수 여부를 판별
+    for num in range(2, int(n ** 0.5) + 1):
+        if is_prime[num]:
+            # num의 배수들은 소수가 아님
+            for i in range(num * num, n + 1, num):
+                is_prime[i] = False
+
+    return sum(is_prime)
